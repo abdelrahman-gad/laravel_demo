d371c0fc8b1715c247e494b0e060c83f828d7b9b
abdelrahman-gad <abdelrahmangad95@gmail.com>
2024-09-08 15:43:14 +0300

    disable schedule

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-  Schedule::command('inspireCustom')->everyMinute();
+//   Schedule::command('inspireCustom')->everyMinute();
